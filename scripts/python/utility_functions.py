7066386dabab96070ad6225a1c97038289b0ba98
valanavm <130632254+valanavm@users.noreply.github.com>
2023-04-14 21:36:52 +0530

    Update utility_functions.py

--- scripts/python/utility_functions.py
+++ scripts/python/utility_functions.py
@@ -1,15 +1,9 @@
 from string import Template
 import os
-import cx Oracle
 import csv
 import sys
-from envparse import env
-import 53fs
 from collections import OrderedDict
-import yaml
 import json
-import requests
-import subprocess
 from datetime import datetime
 import logging
 import traceback
@@ -23,9 +17,8 @@ class utility_functions:
     @staticmethod
     def get_code_base_dir():
         curr_dir_nm = os.path.dirname(os.path.realpath(__file__))
-        print (curr_dir_nm)
         while True:
-            if os.path.basename(curr_dir_nm) == 'Spark_conv_recon':
+            if os.path.basename(curr_dir_nm) == 'spark_conv_recon':
                 return curr_dir_nm
                 break
             else:
@@ -36,12 +29,12 @@ class utility_functions:
         common_vars_dict = dict()
         common_vars_dict['o_date'] = sys.argv[1]
         common_vars_dict['grp_nm'] = sys.argv[2]
-        common_vars_dict['system id'] = 46
+        common_vars_dict['system_id'] = 46
         common_vars_dict['code_base_dir'] = cls.get_code_base_dir()
-        common_vars_dict['data_lkp_dir'] = (common_vars_dict['code_base_dir'] + "/1kp/")
+        common_vars_dict['data_lkp_dir'] = (common_vars_dict['code_base_dir'] + "/lkp/")
         common_vars_dict['data_log_dir'] = (common_vars_dict['code_base_dir'] + "/log/")
-        common vars_dict['drvr_file'] = (
-            common_vars_dict['data_lkp_dir'] + "driver_list.1kp"
+        common_vars_dict['drvr_file'] = (
+            common_vars_dict['data_lkp_dir'] + "driver_list.lkp"
         )
         common_vars_dict['grp_log_file'] = (
             common_vars_dict['data_log_dir']
@@ -53,14 +46,13 @@ class utility_functions:
             + "_log.txt"
         )
         return common_vars_dict
-
     @classmethod
-    def gen_interface_vars_init(cls, unique_source_tables_set, common_vars_dict, tech fields_dict):
+    def gen_interface_vars_init(cls, unique_source_tables_set, common_vars_dict, tech_fields_dict):
         common_vars_dict['cln_file_nm'] = (
-            "file:///home/grdwbtch/valan/Spark_conv_recon/1kp/"
-            + common_vars_dict'o_date']
+            "file:///home/ec2-user/spark_conv_recon/lkp/"
+            + common_vars_dict['o_date']
             + "_"
-            + common vars_dict['table_nm']
+            + common_vars_dict['table_nm']
             + "_clean_file.csv"
         )
         return common_vars_dict
@@ -75,25 +67,26 @@ class utility_functions:
         unique_source_tables_set = set()
         tech_fields_dict = dict()
         sttm_dict_req_keys_list = [
-            'source column',
+            'source_column',
             'target_column',
             'target_data_type_len',
             'transform_function',
             'target_data_type',
         ]
 
-        with open(sttm_file, 'r') as sttm file:
+        with open(sttm_file, 'r') as sttm_file:
             dict_reader = csv.DictReader(sttm_file)
+            print("-------Working on control structure-------")
             for dict_row in dict_reader:
                 sttm_dict_list.append(
-                {
-                    key: value
-                    for key, value in list(dict_row.items())
-                    if key in sttm_dict_req_keys_list
-                }
+                    {
+                        key: value
+                        for key, value in list(dict_row.items())
+                        if key in sttm_dict_req_keys_list
+                    }
+                )
                 sttm_target_columns_list.append(dict_row['target_column'])
 
-        print(sttm_dict_list)
         sttm_target_columns_list = list(OrderedDict.fromkeys(sttm_target_columns_list))
         return (
             unique_source_tables_set,
