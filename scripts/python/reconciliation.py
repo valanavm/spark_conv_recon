efa7b7b4abc3201f773ffc7eae455208df92fb06
valanavm <130632254+valanavm@users.noreply.github.com>
2023-04-14 21:34:54 +0530

    Update reconciliation.py

--- scripts/python/reconciliation.py
+++ scripts/python/reconciliation.py
@@ -7,24 +7,32 @@ import datacompy
 odate = sys.argv[1]
 table_nm = sys.argv[2]
 
-conv_file_nm = "/home/qrdwbtch/valan/Spark_conv_recon/1kp/"+odate+"_"+table_nm+"_clean file.csv/*.csv"
-source file nm = "/home/qrdwbtch/valan/Spark_conv_recon/1kp/"+odate+"_"+table_nm+"_source_output.csv"
-l = [pd.read_csv(filename) for filename in glob.glob(conv_file_nm)]
-conv_df = pd.concat(1, axis=0)
+conv_file_nm = "/home/ec2-user/spark_conv_recon/lkp/"+odate+"_"+table_nm+"_clean_file.csv/*.csv"
+source_file_nm = "/home/ec2-user/spark_conv_recon/lkp/"+odate+"_"+table_nm+"_source_output.csv"
+l = [pd.read_csv(filename,sep="\001") for filename in glob.glob(conv_file_nm)]
+conv_df = pd.concat(l, axis=0)
 
 source_df = pd.read_csv(source_file_nm, sep=",", header='infer')
+
+print("-----------comparing two files-------------------")
+print(source_df)
+print(conv_df)
+
+print("--------ignoring not neeeded column--------------")
+source_df=source_df.drop(['publn_id'], axis=1)
+conv_df=conv_df.drop(['publn_id'], axis=1)
 
 compare = datacompy.Compare(
             source_df,
             conv_df,
-            join columns=['risk id', 'risk_dmn_cd'],
+            join_columns=['risk_id','risk_dmn_cd'],
             df2_name="expected",
             df1_name="actual",
 )
 
 report = compare.report()
 
-with open(f"/home/qrdwbtch/valan/Spark_conv_recon/1kp/recon.report", "w") as fp:
+with open(f"/home/ec2-user/spark_conv_recon/lkp/recon.report", "w") as fp:
     fp.write(report)
 
 is_match = compare.matches()
@@ -37,17 +45,18 @@ source_only = compare.df1_unq_rows.shape[0]
 target_mismatched_pks = str(exp_pk_sets)
 source_mismatched_pks = str(act_pk_sets)
 
-if not is match:
-    print("Number of mismatches are ", num mismatches)
+if not is_match:
+    print("Number of mismatches are ", num_mismatches)
     print("Below are the mismatch details in the Spark output with the existing platform output")
-    print("Records only in Spark output is ", num_target_only)
-    print("Records only in existing platform output is ", num_source_only)
+    print("Records only in Spark output is ", target_only)
+    print("Records only in existing platform output is ", source_only)
     print("Primary Keys only in Spark output are ", target_mismatched_pks)
     print("Primary Keys only in existing platform are ", source_mismatched_pks)
     print("Saved the complete reconciliation report for your reference")
-    print(report)
 else:
-    print("Number of mismatches are ", num mismatches)
+    print("Number of mismatches are ", num_mismatches)
     print("There are no mismatches in Spark output with the existing platform output")
     print("Saved the complete reconciliation report for your reference")
-    print(report)
+
+print("--------printing recon summary report---------")
+print(report)
